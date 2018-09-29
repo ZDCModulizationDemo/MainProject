055274ea7088b54dd3e26ed3842664dea27b35f2
张超范 <3213689@qq.com>
2018-09-29 15:41:41 +0800

    initial project

---
+++ ModulizedMainProject/ModulizedMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModulizedMainProject
+//
+//  Created by 张得丑 on 2018/9/29.
+//  Copyright © 2018年 EvilZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModulizedMainProject/ModulizedMainProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ModulizedMainProject
+//
+//  Created by 张得丑 on 2018/9/29.
+//  Copyright © 2018年 EvilZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
